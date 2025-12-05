cd68dadf32b53a51d8870120913a8d4f7bd073a2
Henrique <henriqueeliashespanhol@gmail.com>
2025-12-05 19:39:23 -0300

    nome db

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,7 +7,7 @@ class Database {
 
     init(){
         this.db = new Sequelize({
-            database: process.env.DBname ?? "FullstackBanco",
+            database: process.env.DBname ?? "fullstackbanco",
             host: process.env.DBhost ?? "dpg-d4plffer433s739f81j0-a",
             username: process.env.DBusername ?? "fullstackbanco_user",
             password: process.env.DBpassword ?? "uIKWcjXaxnAeT5BYY8pISvTg3E4hSoNV",
